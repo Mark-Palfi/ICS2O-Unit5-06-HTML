7844b36860d91f71253545ec188b173c10c64a75
Mark-Palfi <99292598+Mark-Palfi@users.noreply.github.com>
2022-11-02 18:02:21 +0000

    loop edit

--- js/script.js
+++ js/script.js
@@ -8,11 +8,15 @@
  * This function gets users street name and number and shows it back to user.
  */
 function enterClicked() {
+  let answer = 0
+  
   // input
   const factor1 = document.getElementById("factor1").value
   const factor2 = parseInt(document.getElementById("factor2").value)
 
   // output
-  while (product != factor1 * factor2) {
+  for (let counter = 0; counter < factor2; counter++) {
+    answer = answer + factor1
   }
+  text = text + "The answer is " + answer + "<br>";
 }
